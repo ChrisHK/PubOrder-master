871713e016b60b178f27752e15ac2f2638214245
ChrisHK <christo.or@gmail.com>
2020-01-09 21:32:49 +0800

    normal information

--- PubOrder/Properties/AssemblyInfo.cs
+++ PubOrder/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("PubOrder")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("5fb57d37-410e-4b72-8d21-2c45887d3dcf")]
+[assembly: Guid("a1400d29-8e9f-4515-a17c-20780f430884")]
 
 // Version information for an assembly consists of the following four values:
 //
